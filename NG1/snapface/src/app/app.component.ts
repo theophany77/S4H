73f9cb25ee9b15ce9695285cf4bd2f9f3c448dc3
theophany77 <alzingrejm@gmail.com>
2024-11-19 14:57:25 +0000

    commi du 20241119 a 15h57

---
+++ NG1/snapface/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    FaceSnapComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+
+
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap! : FaceSnap;
+  myLastSnap! : FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myOtherSnap = new FaceSnap(
+      'Jesus',
+      'My best friend !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myLastSnap = new FaceSnap(
+      'Gabriel',
+      'L Archange !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+  }
+}
---
+++ NG1/snapface/src/app/models/face-snap.ts
@@ -0,0 +1,16 @@
+export class FaceSnap {
+    constructor(public title: string,
+                public description: string,
+                public imageUrl: string,
+                public createdAt: Date,
+                public snaps: number) {}
+
+    addSnap(): void {
+                    this.snaps++;
+                  }
+                
+    removeSnap(): void {
+                    this.snaps--;
+                  }
+
+    }
